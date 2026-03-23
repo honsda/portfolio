89c984d87bdffe227cae24b53a0c1722acc0b417
honsda <xuehonia@gmail.com>
2026-03-23 18:56:40 +0700

    feat: update asset paths to use dynamic base URL; enhance Footer, Hero, Nav, and Base components for improved asset handling

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://honsda.github.io',
+  base: '/portfolio',
   integrations: [svelte(), tailwind()],
   output: 'static',
 });
